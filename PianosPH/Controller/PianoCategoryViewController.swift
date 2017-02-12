8f1d9ecc046ee80cbc72b1b1498ffdaed935f2f3
Emmanuel Tugado <emmanueltugado@gmail.com>
2017-02-12 12:35:15 +0800

    basic cell

--- PianosPH/Controller/PianoCategoryViewController.swift
+++ PianosPH/Controller/PianoCategoryViewController.swift
@@ -9,27 +9,28 @@
 import UIKit
 
 class PianoCategoryViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private static let pianoCategoryCell = "pianoCategoryCell"
+    
+    var pianoCategories: [String]  = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension PianoCategoryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pianoCategories.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
-
 }
---
+++ PianosPH/View/PianoCategoryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PianoCategoryTableViewCell.swift
+//  PianosPH
+//
+//  Created by Emmanuel Francisco Tugado on 12/2/17.
+//  Copyright © 2017 Emmanuel Tugado. All rights reserved.
+//
+
+import UIKit
+
+class PianoCategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+}
