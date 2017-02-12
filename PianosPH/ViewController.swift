65f8deb0969b66de022546aca8e34c24559d12b4
Emmanuel Tugado <emmanueltugado@gmail.com>
2017-02-12 10:33:59 +0800

    Initial Commit

---
+++ PianosPH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PianosPH
+//
+//  Created by Emmanuel Francisco Tugado on 12/2/17.
+//  Copyright © 2017 Emmanuel Tugado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
